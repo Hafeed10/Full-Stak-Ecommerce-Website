164fc4fe81ca258acbd3fbf575dc0626fa0cf468
Hafeed10 <hafeex@gamil.com>
2025-09-26 23:04:44 +0530

    commit

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,12 +1,15 @@
 import { serve } from "inngest/next";
 import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
 
-// Export the API handlers
+// Prevent Next.js from trying to prerender this API at build time
+export const dynamic = "force-dynamic";
+
+// Register Inngest functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     syncUserCreation,
     syncUserUpdation,
-    syncUserDeletion
+    syncUserDeletion,
   ],
 });
